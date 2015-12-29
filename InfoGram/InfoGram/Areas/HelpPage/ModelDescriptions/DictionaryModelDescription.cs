4c675fb405d24c9a7ffbdb1c2452341207ca3adc
Glendon Ngo <g2ngo@uwaterloo.ca>
2015-12-29 03:20:02 -0500

    Pushing solution to git

---
+++ InfoGram/InfoGram/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InfoGram.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ InfoGram/InfoGram/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(InfoGram.Startup))]
+
+namespace InfoGram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
